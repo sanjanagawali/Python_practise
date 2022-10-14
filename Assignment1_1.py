c1bb854575f4ddc02c0fa7a2d3f5b75f0250b298
Sanjana Gawali <sanjanagawali99@gmail.com>
2022-10-14 22:42:12 +0530

    Add files via upload

---
+++ Assignment1_1.py
@@ -0,0 +1,13 @@
+print("==========Program to display Hello============")
+
+def fun():
+    print("Hello from Fun")
+
+def main():
+    fun()
+
+if __name__ == "__main__":
+    main()
+
+print("==============================================")
+
